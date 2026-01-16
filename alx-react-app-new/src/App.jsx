e77921a9de17c554465cf6470b1091176a5c4b67
Susan Aremo <aremosue@gmail.com>
2026-01-16 12:20:28 +0300

    Move components to src/components and update App.jsx imports

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,17 +1,19 @@
 import UserProfile from './components/UserProfile';
-
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-       <UserProfile 
+      <Header />
+      <UserProfile 
         name="Alice" 
         age={25} 
         bio="Loves hiking and photography" 
       />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
